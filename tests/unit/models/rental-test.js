dfe711c464e586a276874eaea6a45f3ec3af41a8
Alberto Vena <kennyadsl@gmail.com>
2017-01-14 16:28:32 +0100

    Use ember data with rental model

--- tests/unit/models/rental-test.js
+++ tests/unit/models/rental-test.js
@@ -1,6 +1,6 @@
 import { moduleForModel, test } from 'ember-qunit';
 
-moduleForModel('rentals', 'Unit | Model | rentals', {
+moduleForModel('rental', 'Unit | Model | rental', {
   // Specify the other units that are required for this test.
   needs: []
 });
